f33d4b31d8ca6d35aa2a11fdefdac7d88cef8525
Ana <aoliveiramoraes1112@gmail.com>
2025-04-04 15:56:01 -0300

    Primeiro commit

---
+++ api.py
@@ -0,0 +1,96 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+api = Api(app)
+
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User(name={self.name}, email={self.email})"
+
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, required=True, help='Name cannot be blank')
+user_args.add_argument('email', type=str, required=True, help='Email cannot be blank')
+
+userFields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'email': fields.String,
+}
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args["name"], email=args["email"])
+        
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return user, 201  # Retorna o usuário criado com status 201 (Created)
+        
+class User(Resource):
+    @marshal_with(userFields)
+    def get(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, message="User not found")  # Corrigido o abort()
+        return user
+
+
+class User(Resource):
+    @marshal_with(userFields)
+    def patch(self, id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        
+        if not user:
+            abort(404, message="User not found")
+        
+        # Atualiza os campos
+        if args['name']:
+            user.name = args['name']
+        if args['email']:
+            user.email = args['email']
+        
+        db.session.commit()
+        return user
+
+    @marshal_with(userFields)
+    def delete(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        
+        if not user:
+            abort(404, message="User not found")
+        
+        db.session.delete(user)
+        db.session.commit()
+        
+        return 'user delete', 204 
+          
+    
+api.add_resource(Users, '/api/users')
+api.add_resource(User, '/api/users/<int:id>') 
+
+@app.route('/')
+def home():
+    return '<h1>Flask REST API</h1>'
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Garante que o banco de dados seja criado
+    app.run(debug=True)
